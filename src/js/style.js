ec2f97194c1fe88c8cd6f62d2b4ce698ef61328e
Minh Nguyễn <mxn@1ec5.org>
2024-03-23 23:05:59 -0700

    🍋

--- src/js/style.js
+++ src/js/style.js
@@ -30,6 +30,31 @@ export function build(tileURL, spriteURL, glyphURL, locales) {
                 "type": "Point",
                 "coordinates": [-116.3694769,33.7667042]
               }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "Limón",
+                "name:en": "Limon",
+                "name:es": "Limón",
+                "name:uk": "Лаймон",
+                "name:sr": "Лимон",
+                "name:kk": "Лимон",
+                "name:arz": "ليمون",
+                "name:ce": "Лаймон",
+                "name:mr": "लायमन",
+                "name:ky": "Лимон",
+                "name:zh": "利蒙",
+                "name:ar": "ليمون",
+                "name:fa": "لیمون",
+                "name:tt": "Лаймон",
+                "name:bg": "Лаймън",
+                "capital": 4,
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [-103.6931230,39.2648545]
+              }
             }
           ]
         } : {}
