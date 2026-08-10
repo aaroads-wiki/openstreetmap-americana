c9a0cc8ff834b5fc2653f5732f14b03001f70e17
Minh Nguyễn <mxn@1ec5.org>
2026-08-09 19:37:55 -0700

    Formatted GeoJSON

--- src/js/style.ts
+++ src/js/style.ts
@@ -34,49 +34,51 @@ export function build(
       },
       controlcities: {
         type: "geojson",
-        data: fool ? {
-          "type": "FeatureCollection",
-          "features": [
-            {
-              "type": "Feature",
-              "properties": {
-                "name": "Other Desert Cities",
-                "name:en": "Other Desert Cities",
-                "name:ja": "アザー・デザート・シティーズ",
-                "name:zh": "其他沙漠城市",
-              },
-              "geometry": {
-                "type": "Point",
-                "coordinates": [-116.3694769,33.7667042]
-              }
-            },
-            {
-              "type": "Feature",
-              "properties": {
-                "name": "Limón",
-                "name:en": "Limon",
-                "name:es": "Limón",
-                "name:uk": "Лаймон",
-                "name:sr": "Лимон",
-                "name:kk": "Лимон",
-                "name:arz": "ليمون",
-                "name:ce": "Лаймон",
-                "name:mr": "लायमन",
-                "name:ky": "Лимон",
-                "name:zh": "利蒙",
-                "name:ar": "ليمون",
-                "name:fa": "لیمون",
-                "name:tt": "Лаймон",
-                "name:bg": "Лаймън",
-                "capital": 4,
-              },
-              "geometry": {
-                "type": "Point",
-                "coordinates": [-103.6931230,39.2648545]
-              }
+        data: fool
+          ? {
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
+                  properties: {
+                    name: "Other Desert Cities",
+                    "name:en": "Other Desert Cities",
+                    "name:ja": "アザー・デザート・シティーズ",
+                    "name:zh": "其他沙漠城市",
+                  },
+                  geometry: {
+                    type: "Point",
+                    coordinates: [-116.3694769, 33.7667042],
+                  },
+                },
+                {
+                  type: "Feature",
+                  properties: {
+                    name: "Limón",
+                    "name:en": "Limon",
+                    "name:es": "Limón",
+                    "name:uk": "Лаймон",
+                    "name:sr": "Лимон",
+                    "name:kk": "Лимон",
+                    "name:arz": "ليمون",
+                    "name:ce": "Лаймон",
+                    "name:mr": "लायमन",
+                    "name:ky": "Лимон",
+                    "name:zh": "利蒙",
+                    "name:ar": "ليمون",
+                    "name:fa": "لیمون",
+                    "name:tt": "Лаймон",
+                    "name:bg": "Лаймън",
+                    capital: 4,
+                  },
+                  geometry: {
+                    type: "Point",
+                    coordinates: [-103.693123, 39.2648545],
+                  },
+                },
+              ],
             }
-          ]
-        } : {}
+          : {},
       },
     },
     sprite: spriteURL,
